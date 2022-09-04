b874ad3690d3fc273e1631b4d0babd5be4547706
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2022-09-04 20:37:38 +0200

    Install laravel hyperpay package
    - Use hyperpay trait in user model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Devinweb\LaravelHyperpay\Traits\ManageUserTransactions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-	use HasApiTokens, HasFactory, Notifiable;
+	use HasApiTokens, HasFactory, Notifiable, ManageUserTransactions;
 
 	/**
 	 * The attributes that are mass assignable.
---
+++ database/migrations/2022_09_03_123638_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('transactions', function (Blueprint $table) {
+			$table->string('id')->primary();
+			$table->integer('user_id');
+			$table->string('checkout_id')->unique();
+			$table->string('status');
+			$table->float('amount');
+			$table->string('currency');
+			$table->json('data')->nullable();
+			$table->json('trackable_data');
+			$table->string('brand');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('transactions');
+	}
+}
